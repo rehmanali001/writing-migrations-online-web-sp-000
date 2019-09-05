122576c3e287d059c02029503e5d2216c6ecdc4c
Rehman Ali <rehmanali001@gmail.com>
2019-09-05 19:26:18 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudent < ActiveRecord::Migration[5.1]
   def change 
-    add_column :students, :grade, :Integer, 
+    add_column :students, :grade, :Integer
     add_column :students, :birthdate, :String
   end
 end
